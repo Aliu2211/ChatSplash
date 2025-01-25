07a6ca8fafad2277c07d960b04142bba2d171111
Aliu Tijani <aliutijani21@gmail.com>
2025-01-25 04:35:53 +0000

    updated

--- App.tsx
+++ App.tsx
@@ -2,14 +2,20 @@
 //import liraries
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import Main from './src/navigation/main';
+import NavigationStack from './src/App';
 
 // create a component
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Main />
-    </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+        <View style={styles.container}>
+          {/* <Main /> */}
+          <NavigationStack/>
+          
+        </View>
+    </GestureHandlerRootView>
   );
 };
 
